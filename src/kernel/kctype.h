2bb7e4be0b7b4e777a92b5b3324ecf24e6789ade
shardofgl4ss <133802659+shardofgl4ss@users.noreply.github.com>
2026-08-22 20:12:36 -0400

    remove useless attribute

--- src/kernel/kctype.h
+++ src/kernel/kctype.h
@@ -6,7 +6,7 @@
 
 #include "types.h"
 
-#define _CTYPE static inline __attribute__((const, always_inline, used))
+#define _CTYPE static inline __attribute__((const, always_inline))
 
 /* These do not explicitly comply with POSIX standards as they take u8's. ------ *
  * But this is only used inside the kernel anyway. As long as they do what ----- *
