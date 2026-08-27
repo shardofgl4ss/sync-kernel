67807bb9f81041f6ca11beadf2d13d866acaa903
shardofgl4ss <133802659+shardofgl4ss@users.noreply.github.com>
2026-08-27 08:18:19 -0400

    implement buddy allocator (probably still full of bugs)!!

--- src/kernel/pframe.c
+++ src/kernel/pframe.c
@@ -1,6 +1,8 @@
 // SPDX-License-Identifier: GPL-3.0-only
 // Copyright (C) 2026 Sync Shard
 
+#include "pframe.h"
+
 #include "boot.h"
 #include "kstring.h"
 #include "macros.h"
@@ -9,140 +11,424 @@
 #include "panic.h"
 #include "types.h"
 
+#include "builtin.h"
 
 extern char _kphys_start[];
 extern char _kphys_end[];
 
-struct kpage_core *phys_core = NULL;
+master_buddy_t MASTER_BUDDY;
+
+#define ASSERT_FUNC(str)     panic(str)
+
+#ifdef DEBUG
+#define ASSERT(expr, str) \
+        do { \
+                if (unlikely(!(expr))) { \
+                        ASSERT_FUNC(str); \
+                } \
+        } while (0)
+#else
+#define ASSERT(expr, str)
+
+#endif
+
+#define MIN(a, b)       ((a) < (b)) ? (a) : (b)
 
 
-static void pfa_region_init()
+_const_ _always_inline_
+static inline usize kbud_order_idx(usize n)
 {
-        struct multiboot_tag_mmap *map = multiboot2_tmmap;
-        if (unlikely(!map)) {
-                panic("GRUB memory map was null!");
-        }
-
-        // const u64 kernsz = (u64)_kphys_end - (u64)_kphys_start;
-
-        int i = 0;
-        const u8 *end = (u8 *)map + map->size;
-
-        for (u8 *p = (u8 *)map + sizeof(*map); p < end; p += map->entry_size) {
-                multiboot_memory_map_t *e = (void *)p;
-
-                if (e->type != MULTIBOOT_MEMORY_AVAILABLE)
-                        continue;
-
-                physmem_region_t *r = &phys_core->region[i++];
-
-                /* shouldn't remap the preinit region */
-                if ((void *)e->addr == r->base)
-                        continue;
-
-                const usize total_regionsz = sizeof(physmem_region_t) * phys_core->regions;
-                if (unlikely(offsetof(struct kpage_core, region) + total_regionsz >= PAGESIZE)) {
-			panic("system memory mapping has too many regions!");
-		}
-
-                r->base = (void *)e->addr;
-
-                /* e->len is probably never malformed, but just in case, we round down */
-                r->max_frames = (e->len & ~(PAGESIZE - 1)) / PAGESIZE;
-                r->cur_frames = r->max_frames;
-                r->top = NULL;
-
-                phys_core->regions++;
-        }
+        return count_trailing_zeroes(n) - 12;
 }
 
 
 
 
-void kphys_alloc_early_init(void)
+/* Call check_valid_sbuddy() instead during init. this only checks preceeding. */
+static slave_buddy_t *find_buddy_slave(const phys_addr_t addr)
 {
-        extern struct kpage_core preinit_pfa;
-        // 0 is preinit region.
-        physmem_region_t *ppf = &preinit_pfa.region[0];
-
-        if (unlikely(ppf->max_frames == 0 || ppf->cur_frames == 0)) {
-                panic("the impossible happened: zero pages in preinit pages!");
+        if (unlikely(MASTER_BUDDY.region_count == 0 
+            || MASTER_BUDDY.region_count > PFRAME_MAX_REGIONS)) {
+                return KPAGE_ERR;
         }
 
+        isize lo = 0;
+        isize hi = MASTER_BUDDY.region_count - 1;
+        isize owner = -1;
 
-        phys_core = (void *)ppf->top;
-        page_frame_t *top = ppf->top - 1;
+        while (lo <= hi) {
+                isize mid = (lo + hi) / 2;
 
-        memset(phys_core, 1, PAGESIZE);
-
-        physmem_region_t *rg = &phys_core->region[0];
-
-        rg->base = ppf->base;
-	rg->top = top;
-        rg->max_frames = ppf->max_frames;
-        rg->cur_frames = ppf->cur_frames - 1;
-
-	phys_core->regions = 1;
-        pfa_region_init();
-}
-
-
-
-
-/* only allocates from region 0 of phys_core, the same region used in preinit - *
- * warning: this is an internal call. the pages returned may not be mapped ---- */
-void *_kphys_pre_alloc(usize pages)
-{
-        physmem_region_t *rg = &phys_core->region[0];
-
-        if (pages > rg->cur_frames) 
-                return NULL;
-
-        usize pagerq = pages;
-        while (pagerq--) {
-                rg->top = rg->top->next;
-        }
-
-        page_frame_t *a = rg->top;
-        rg->cur_frames -= pages;
-
-        return a;
-}
-
-
-
-
-/* !!! warning: this is an internal call. the pages returned may not be mapped. !!! */
-__attribute__((hot)) //
-void *_kphys_alloc(usize pages)
-{
-        physmem_region_t *rg;
-        for (usize i = 0; i < phys_core->regions; i++) {
-                rg = &phys_core->region[i];
-
-                if (rg->cur_frames >= pages) {
-                        goto alloc;
+                if ((phys_addr_t)MASTER_BUDDY.regions[mid] <= addr) {
+                        owner = mid;
+                        lo = mid + 1;
+                } else {
+                        hi = mid - 1;
                 }
         }
-        return NULL;
-alloc:
-        usize pagerq = pages;
-        while (pagerq--) {
-                rg->top = rg->top->next;
-        }
 
-        page_frame_t *a = rg->top;
-        rg->cur_frames -= pages;
-
-        return a;
+        return owner != -1 ? MASTER_BUDDY.regions[owner] : KPAGE_ERR;
 }
 
 
 
 
-void kphys_alloc_init(void)
+static slave_buddy_t *find_valid_slave(const phys_addr_t addr)
 {
-        /* TODO: possibly implement a buddy allocator. */
-        kphys_alloc_early_init();
+        slave_buddy_t *s = find_buddy_slave(addr);
+
+        if (s == KPAGE_ERR || addr >= s->mbase + (s->total_pages * PAGESIZE)) {
+                return KPAGE_ERR;
+        }
+
+        return s;
+}
+
+
+
+
+_cold_
+static inline void kbud_mark_invalid(slave_buddy_t *region)
+{
+        memset(region, (u8)-1, sizeof(*region));
+        region->rbase = (phys_addr_t)region;
+        region->total_pages = 0;
+}
+
+
+
+
+_cold_
+static void kbud_init_region(slave_buddy_t *region, const usize len)
+{
+        /* It is 64 bytes to allow the compiler to vectorize.           *
+         * The bigger this number is, the more waste bytes there is.    */
+        static constexpr usize BITMAP_ALIGN = 0x40;
+
+        const usize rpages = PAGE_ALIGNDOWN(len) >> 12;
+        region->total_pages = rpages;
+        region->rbase = (phys_addr_t)region;
+
+        usize total_bytes = 0;
+        /* cur is used as an aligned bitmap base offset, and it can be  *
+         * used for the end of the last bitmap too for setting mbase.   */
+        u8 *cur = (u8 *)(((region->rbase + sizeof(*region)) + 7) & ~(usize)7);
+
+        for (usize i = 0; i < PFRAME_MAX_ORDER; i++) {
+                const usize bits = rpages >> (i + 1);
+                if (unlikely(!bits)) break;
+
+                const usize bytes = (bits + 7) >> (usize)3;
+
+                region->lut.bitmap_arr[i] = cur;
+                region->lut.maxbits[i] = bits;
+                region->max_order = (bud_order_t)i;
+                total_bytes += SY_ALIGN_UP(bytes, BITMAP_ALIGN);
+                cur += SY_ALIGN_UP(bytes, BITMAP_ALIGN);
+        }
+
+
+        region->lut.base = (phys_addr_t)((u8 *)region + sizeof(*region));
+        region->mbase = PAGE_ALIGNUP((phys_addr_t)cur);
+
+        /* if mbase is ever equal or greater than               *
+         * region + len, that means the tiny bitmap             *
+         * alone swallowed all the space, its a useless region. */
+        if (unlikely(region->mbase >= (phys_addr_t)region + len)) {
+                goto err;
+        }
+
+        const phys_addr_t end = region->rbase + PAGESIZE;
+        region->lut.bitmap_reserved = (region->mbase > end)
+                ? (region->mbase - end) >> 12
+                : 0;
+
+        region->total_pages = ((phys_addr_t)region + len - region->mbase) >> 12;
+        memset((u8 *)region + sizeof(*region), 0, (phys_addr_t)cur - (phys_addr_t)region);
+
+        return;
+err:
+        kbud_mark_invalid(region);
+}
+
+
+
+
+slave_buddy_t *kbud_add_region(const phys_addr_t addr, const usize len)
+{
+        if (unlikely(MASTER_BUDDY.region_count + 1 > PFRAME_MAX_REGIONS)) {
+                return KPAGE_ERR;
+        }
+
+        usize *rc = &MASTER_BUDDY.region_count;
+        slave_buddy_t **mbr = MASTER_BUDDY.regions;
+
+        usize i = *rc;
+        while (i > 0 && mbr[i - 1] > (slave_buddy_t *)addr) {
+                mbr[i] = mbr[i - 1];
+                i--;
+        }
+
+        mbr[i] = (slave_buddy_t *)addr;
+        mbr[++(*rc)] = KPAGE_ERR;
+
+        memset(mbr[i], 0, sizeof(*mbr[i]));
+        kbud_init_region(mbr[i], len);
+
+        return mbr[i];
+}
+
+
+
+
+_const_
+static inline u8 kbud_byte_order(usize sz)
+{
+        const usize frames = (sz + PAGESIZE - 1) >> 12;
+        if (frames <= 1) return 0;
+        return (u8)(63 - count_leading_zeroes(frames - 1)) + 1;
+}
+
+
+_reproducible_
+static inline phys_addr_t kbud_pfnum(const phys_addr_t addr, slave_buddy_t *s)
+{
+        return (addr - s->mbase) >> 12;
+}
+
+_const_
+static inline usize kbud_pair_idx(const phys_addr_t pfn, bud_order_t ord)
+{
+        return (usize)(pfn >> (ord + 1));
+}
+
+
+/* checks for a valid bit from pfn in slave, returns false if not valid */
+_reproducible_ _nonnull_(1)
+static inline bool kbuddy_bit_chk(slave_buddy_t *s, phys_addr_t pfn, bud_order_t ord)
+{
+        if (ord > s->max_order) return false;
+        const usize pidx = kbud_pair_idx(pfn, ord);
+        if (pidx >= s->lut.maxbits[ord]) return false;
+
+        return true;
+}
+
+_reproducible_ _nonnull_(1)
+static inline bool kbuddy_bit_get(slave_buddy_t *s, phys_addr_t pfn, bud_order_t ord) 
+{
+        const usize pidx = kbud_pair_idx(pfn, ord);
+
+        ASSERT(ord > s->max_order || pidx >= s->lut.maxbits[ord], 
+               "attempt to access invalid bit in kbuddy!");
+
+        return (s->lut.bitmap_arr[ord][pidx >> 3] >> (pidx & 7)) & 1;
+}
+
+_nonnull_(1)
+static inline bool kbuddy_bit_set(slave_buddy_t *s, phys_addr_t pfn, bud_order_t ord)
+{
+        const usize pidx = kbud_pair_idx(pfn, ord);
+
+        ASSERT(ord > s->max_order || pidx >= s->lut.maxbits[ord], 
+               "attempt to mutate invalid bit in kbuddy!");
+
+        return (s->lut.bitmap_arr[ord][pidx >> 3] |= (u8)(1 << (pidx & 7)));
+}
+
+_nonnull_(1)
+static inline bool kbuddy_bit_clr(slave_buddy_t *s, phys_addr_t pfn, bud_order_t ord)
+{
+        const usize pidx = kbud_pair_idx(pfn, ord);
+
+        ASSERT(ord > s->max_order || pidx >= s->lut.maxbits[ord], 
+               "attempt to mutate invalid bit in kbuddy!");
+
+        return (s->lut.bitmap_arr[ord][pidx >> 3] &= (u8)~(1 << (pidx & 7)));
+}
+
+
+
+
+/* returns true if push was successful, false on error. */
+static bool kbud_push(slave_buddy_t *slave, void *addr, bud_order_t order)
+{
+        phys_addr_t pfn = kbud_pfnum((phys_addr_t)addr, slave);
+        if (unlikely(!kbuddy_bit_chk(slave, pfn, order))) {
+                return false;
+        }
+
+        page_frame_t *p = addr;
+
+        p->order = order;
+        p->next = slave->free[order];
+        slave->free[order] = p;
+        slave->order_blk_cnt[order]++;
+
+        kbuddy_bit_set(slave, pfn, order);
+
+        return true;
+}
+
+/* returns true if pop was successful, false on error. */
+static bool kbud_pop(slave_buddy_t *slave, void *addr, bud_order_t order)
+{
+        phys_addr_t pfn = kbud_pfnum((phys_addr_t)addr, slave);
+        if (unlikely(!kbuddy_bit_chk(slave, pfn, order) 
+         || unlikely(slave->order_blk_cnt[order] == 0))) {
+                return false;
+        }
+
+        /* the linus torvalds algorithm :p */
+        page_frame_t **cur = &slave->free[order];
+
+        while (*cur != (page_frame_t *)addr) {
+                cur = &(*cur)->next;
+        }
+
+        *cur = (*cur)->next;
+        slave->order_blk_cnt[order]--;
+
+        kbuddy_bit_clr(slave, pfn, order);
+
+        return true;
+}
+
+/* returns valid ptr if pop was successful, KPAGE_ERR on error. */
+static void *kbud_pop_head(slave_buddy_t *slave, bud_order_t order)
+{
+        page_frame_t *blk = slave->free[order];
+
+        phys_addr_t pfn = kbud_pfnum((phys_addr_t)blk, slave);
+        if (unlikely(!kbuddy_bit_chk(slave, pfn, order))) {
+                return KPAGE_ERR;
+        }
+
+
+        slave->free[order] = blk->next;
+        slave->order_blk_cnt[order]--;
+
+        kbuddy_bit_clr(slave, pfn, order);
+        
+        return blk;
+}
+
+
+
+
+void kbud_free_range(const phys_addr_t phys, const usize len)
+{
+        phys_addr_t addr = PAGE_ALIGNDOWN((phys_addr_t)phys);
+
+        slave_buddy_t *s = find_valid_slave(phys);
+        usize size       = PAGE_ALIGNDOWN(len);
+
+        if (unlikely(s == KPAGE_ERR)) {
+                s = kbud_add_region(addr, len);
+                if (unlikely(s == KPAGE_ERR)) return;
+
+                phys_addr_t offs = ((s->mbase - s->rbase) + (PAGESIZE - 1)) & ~(PAGESIZE - 1);
+                addr += offs;
+                size -= offs;
+        }
+
+        static constexpr bud_order_t max_order = PFRAME_MAX_ORDER - 1;
+
+        while (size > 0) {
+                u8 len_order = (u8)(63 - count_leading_zeroes(size / PAGESIZE));
+                u8 ptr_order = (u8)kbud_order_idx((usize)addr);
+
+                u8 ord = MIN(len_order, ptr_order);
+                ord = MIN(max_order, ord);
+                ord = MIN(s->max_order, ord);
+
+                kbud_push(s, (void *)addr, ord);
+
+                const usize blksz = (usize)PAGESIZE << ord;
+                addr += blksz;
+                size -= blksz;
+        }
+}
+
+
+
+
+/* allocates at most size PFRAME_MAX_ORDER - 1, returns KPAGE_ERR on error. */
+_hot_
+void *kbud_allocate(usize sz)
+{
+        const usize rounded_sz = PAGE_ALIGNUP(sz);
+        const u8 req = kbud_byte_order(rounded_sz);
+
+        if (req >= PFRAME_MAX_ORDER) {
+                return KPAGE_ERR;
+        }
+
+        for (usize i = 0; i < MASTER_BUDDY.region_count; i++) {
+                slave_buddy_t *s = MASTER_BUDDY.regions[i];
+                
+                if (unlikely(s == KPAGE_ERR)) {
+                        continue;
+                }
+
+                u8 order = req;
+                while (order < PFRAME_MAX_ORDER 
+                                && s->order_blk_cnt[order] == 0 
+                                && order < s->max_order) {
+                        order++;
+                }
+
+                if (order == PFRAME_MAX_ORDER || order < req) {
+                        continue;
+                }
+
+                void *addr = kbud_pop_head(s, order);
+
+                if (unlikely(addr == KPAGE_ERR)) continue;
+
+                while (order > req) {
+                        const phys_addr_t order_shift = (phys_addr_t)PAGESIZE << order++;
+                        void *buddy = (void *)((phys_addr_t)addr ^ order_shift);
+
+                        kbud_push(s, buddy, order);
+                }
+
+                return addr;
+        }
+
+        return KPAGE_ERR;
+}
+
+
+
+
+_hot_
+void kbud_free(void *addr, bud_order_t order)
+{
+        slave_buddy_t *s = find_valid_slave((phys_addr_t)addr);
+
+        if (unlikely(s == KPAGE_ERR)) 
+                return;
+
+        phys_addr_t pfn = kbud_pfnum((phys_addr_t)addr, s);
+        bud_order_t k = order;
+
+        while (k < PFRAME_MAX_ORDER - 1) {
+                phys_addr_t bpfn = pfn ^ (1ULL << k);
+
+                if (bpfn >= s->total_pages) break;
+                if (!kbuddy_bit_chk(s, bpfn, k)) break;
+                if (!kbuddy_bit_get(s, bpfn, k)) break;
+
+                void *buddy = (void *)(s->mbase + (bpfn << 12));
+
+                kbud_pop(s, buddy, k);
+                kbuddy_bit_clr(s, bpfn, k);
+
+                pfn &= ~(1ULL << k);
+                k++;
+        }
+
+        kbud_push(s, (void *)(s->mbase + (pfn << 12)), k);
 }
 
--- src/kernel/pframe.h
+++ src/kernel/pframe.h
@@ -3,37 +3,92 @@
 
 #pragma once
 
+#include "macros.h"
 #include "types.h"
 #include "pagetbl.h"
+#include "builtin.h"
+
+#define PFRAME_MAX_REGIONS      128
+#define PFRAME_MAX_ORDER        11
+
+typedef uintptr_t phys_addr_t;
+
+typedef enum buddy_page_order {
+        BUDDY_ORDER_4K = 0,
+        BUDDY_ORDER_8K = 1,
+        BUDDY_ORDER_16K = 2,
+        BUDDY_ORDER_32K = 3,
+        BUDDY_ORDER_64K = 4,
+        BUDDY_ORDER_128K = 5,
+        BUDDY_ORDER_256K = 6,
+        BUDDY_ORDER_512K = 7,
+        BUDDY_ORDER_1024K = 8,
+        BUDDY_ORDER_2048K = 9,
+        BUDDY_ORDER_4096K = 10,
 
 
+        BUDDY_ORDER_1M = 8,
+        BUDDY_ORDER_2M = 9,
+        BUDDY_ORDER_4M = 10,
+} bud_order_t;
+
+
+/* this is metadata, but it isn't just metadata.
+ * making it pagealigned makes it easy to do whole-page 
+ * pointer arithmetic. it's basically a representation of a whole page. */
 typedef struct _kpage_frame {
         struct _kpage_frame *next;
+
+        /* future someone: as long as order can go upto MAX_ORDER,      *
+         * its fine to shorten this and add bitfields.                  */
+        u8 order;
 } _PAGEALIGNED page_frame_t;
 
-typedef struct kphysregion {
-        void *base;
-        page_frame_t *top;
-        u64 max_frames;
-        u64 cur_frames;
-} physmem_region_t;
 
-struct kpage_core {
+/* also known as the region header. this has approximately 3896 bytes of extra  *
+ * usable space at the time of writing this comment, for future expansion ----- */
+typedef struct _kslave_buddy {
+        page_frame_t *free[PFRAME_MAX_ORDER];
+        u64 order_blk_cnt[PFRAME_MAX_ORDER];
+        bud_order_t max_order;
+
+        struct {
+                u8 *bitmap_arr[PFRAME_MAX_ORDER];
+                usize maxbits[PFRAME_MAX_ORDER];
+                usize bitmap_reserved;
+                phys_addr_t base;
+
+        } lut;
+
+        phys_addr_t rbase;      /**< base of the whole region --------- */
+        phys_addr_t mbase;      /**< base of the usable region memory - */
+        usize total_pages;
+} slave_buddy_t;
+
+static_assert(sizeof(page_frame_t) == PAGESIZE, "page_frame_t not equal to size of page!\n");
+static_assert(sizeof(slave_buddy_t) <= PAGESIZE, "slave_buddy_t larger than size of page!\n");
+
+/* for future someone who may want to check how much    *
+ * space is left via code hover LSP or similar.         */
+static constexpr i64 KPFRAME_REMAINING_SLAVE_SZ = PAGESIZE - sizeof(slave_buddy_t);
+
+
+/* sadly, master_buddy has no buddies. what a loser. */
+typedef struct _kmaster_buddy {
+        slave_buddy_t *regions[PFRAME_MAX_REGIONS];
+
+        usize region_count;
+        // total ram includes holes like NVS, BADRAM and reserved. (should this even exist?)
+        usize total_ram;
+        // usable ram is self explanatory, only ram regions marked available.
         usize usable_ram;
-        usize regions;
-        physmem_region_t region[];
-} _PAGEALIGNED;
-
-_Static_assert(sizeof(struct kpage_core) == PAGESIZE,
-                "kpage_core not equal to size of page!\n");
-
-extern struct kpage_core *phys_core;
+} master_buddy_t;
 
 
+extern master_buddy_t MASTER_BUDDY;
 
-extern void kphys_alloc_init(void);
 
-/* allocates a count of physical pages, returns a physical address ptr. */
-__attribute__((hot)) //
-extern void *_kphys_alloc(usize pages);
+void *kbud_allocate(usize sz);
+void kbud_free(void *addr, bud_order_t order);
+extern void kbud_free_range(const phys_addr_t phys, const usize len);
 
