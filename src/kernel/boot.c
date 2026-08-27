fef55ae6d765426349479c78e7c8ab5281eec5ca
shardofgl4ss <133802659+shardofgl4ss@users.noreply.github.com>
2026-08-27 08:16:19 -0400

    redo boot.c again (for proper support with primary kernel alloc handoff)

--- src/kernel/boot.c
+++ src/kernel/boot.c
@@ -7,21 +7,17 @@
 #include "types.h"
 #include "pagetbl.h"
 #include "pframe.h"
+#include "kstring.h"
 
 #include <multiboot2.h>
-
-
-#define _PREINIT_               __attribute__((section(".preinit")))
-#define _PREINIT_DATA_          __attribute__((section(".preinit.data")))
-#define _PREINIT_BSS_           __attribute__((section(".preinit.bss")))
 
 
 typedef struct multiboot_mmap_entry mapentry_t;
 extern _Noreturn void kernel_main(void);
 
-_PREINIT_BSS_
-struct kpage_core preinit_pfa = {};
-_PREINIT_BSS_
+_preinit_zeroed_
+struct preinit_allocator preinit_pfa = {};
+_preinit_zeroed_
 struct multiboot_tag_mmap *multiboot2_tmmap = NULL;
 
 extern char _kphys_start[];
@@ -35,7 +31,7 @@ extern char _kheap[];
 static constexpr uint8_t PT_FLAGS_RW = PT_FLAG_PRESENT | PT_FLAG_WRITABLE;
 
 
-_PREINIT_
+_preinit_ _hot_
 static void pi_memset(void *dst, u8 c, usize n)
 {
         u8 *dest = dst;
@@ -45,15 +41,17 @@ static void pi_memset(void *dst, u8 c, usize n)
 }
 
 
-_PREINIT_
-_Noreturn static inline void die(void)
+/* errstr is entirely just for any debugger to see. */
+_preinit_ _noreturn_
+static inline void die(char *errstr)
 {
+        (void)errstr;
         __asm__ volatile ("cli"); 
         for (;;) __asm__ volatile ("hlt");
 }
 
 
-_PREINIT_
+_preinit_
 static inline void debug(void)
 {
         __asm__ volatile (
@@ -64,7 +62,7 @@ static inline void debug(void)
 }
 
 
-_PREINIT_
+_preinit_
 static inline void set_pagetable(PageTable *pml4)
 {
         __asm__ volatile (
@@ -76,8 +74,8 @@ static inline void set_pagetable(PageTable *pml4)
 }
 
 
-_PREINIT_ __attribute__((noreturn, naked))
-_Noreturn static void trampoline()
+_preinit_ _noreturn_ _naked_
+static void trampoline()
 {
         __asm__ volatile (
                 "movq $kstack_top, %rsp\n\t"
@@ -91,7 +89,7 @@ _Noreturn static void trampoline()
 
 
 
-_PREINIT_
+_preinit_
 static struct multiboot_tag *find_mbt(void *mbh, u32 type)
 {
         u8 *p = (u8 *)mbh + 8;
@@ -117,124 +115,126 @@ static struct multiboot_tag *find_mbt(void *mbh, u32 type)
 
 
 
-_PREINIT_
+_preinit_
 static void pf_init_region(const usize region, const mapentry_t *restrict ent)
 {
-        physmem_region_t *r = &preinit_pfa.region[region];
-
         const u64 entry_start = ent->addr;
         const u64 entry_end   = ent->addr + ent->len;
-        const u64 ksize = (u64)_kphys_end - (u64)_kphys_start;
+        const u64 kernel_sz   = (u64)_kphys_end - (u64)_kphys_start;
 
+        void *addr = (void *)ent->addr;
+        usize len = ent->len;
 
         if (unlikely(entry_start <= (u64)_kphys_start
-                    && entry_end >= (u64)_kphys_end))
-        {
-                r->base = (void *)(ent->addr + ksize);
-                r->max_frames = (ent->len - ksize) / PAGESIZE;
-        } else {
-                r->base = (void *)ent->addr;
-                r->max_frames = ent->len / PAGESIZE;
+                    && entry_end >= (u64)_kphys_end)) {
+                addr = (u8 *)addr + kernel_sz;
+                len -= kernel_sz;
         }
 
+        preinit_pfa.region[region] = addr;
+        physmem_pregion_t *r = preinit_pfa.region[region];
 
-        r->cur_frames = r->max_frames;
-        r->top = NULL;
-        preinit_pfa.regions++;
+        r->base = r;
+        r->max_frames = (PAGE_ALIGNDOWN(len)) / 4096;
+
+        /* we need to reserve the first frame for the slave_buddy_t */
+        r->cur_frames = r->max_frames - 1;
+        r->top = ((page_frame_t *)((u8 *)r->base + len)) - 1;
+
+        preinit_pfa.region_count++;
 }
 
 
 
 
-_PREINIT_
-static void alloc_preinit(struct multiboot_tag_mmap *map)
+_preinit_
+static void sort_region_addrs(physmem_pregion_t **r, usize up_to)
 {
-        if (unlikely(map == NULL)) die();
+        for (usize i = 1; i < up_to; i++) {
+                physmem_pregion_t *k = r[i];
+                usize j = i;
 
-        static constexpr usize min_ram_sz = 1024 * 128;
+                while (j > 0 && r[j - 1]->base > k->base) {
+                        r[j] = r[j - 1];
+                        j--;
+                }
 
+                r[j] = k;
+        }
+}
+
+
+
+
+_preinit_
+static void alloc_init(struct multiboot_tag_mmap *map)
+{
+        if (unlikely(map == NULL)) die("map is NULL at alloc_init()");
+
+        pi_memset(preinit_pfa.region, (u8)-1, sizeof(preinit_pfa.region));
+
+        u8 *begin = (u8 *)map + sizeof(*map);
         const u8 *end = (u8 *)map + map->size;
-        usize *ramsz  = &preinit_pfa.usable_ram;
-        bool done     = false;
 
-        for (u8 *p = (u8 *)map + sizeof(*map); p < end; p += map->entry_size) {
-                const mapentry_t *restrict ent = (void *)p;
+        int regions = 0;
+        for (u8 *p = begin; p < end; p += map->entry_size) {
+                const mapentry_t *const ent = (void *)p;
+
+                preinit_pfa.total_ram += ent->len;
 
                 if (ent->type == MULTIBOOT_MEMORY_ACPI_RECLAIMABLE) {
-                        *ramsz += ent->len;
-                        continue;
+                        preinit_pfa.usable_ram += ent->len;
                 }
 
                 if (ent->type != MULTIBOOT_MEMORY_AVAILABLE) {
                         continue;
                 }
 
-                *ramsz += ent->len;
-                /* we should keep looping through entries to get the maximum ram only once.  */
-                if (done) continue;
+                preinit_pfa.usable_ram += ent->len;
 
-                /* The kernel needs at minimum 128kb of extra space. starting with less is shit. */
-                if (ent->len < min_ram_sz) {
-                        continue;
+                pf_init_region(regions, ent);
+                
+                // cant remember if this is at the limit or one off. oh well.
+                if (++regions > PFRAME_MAX_REGIONS - 1) {
+                        die("preinit memory regions exceeded capacity");
                 }
-
-                pf_init_region(0, ent);
-                done = true;
         }
-
-        if (preinit_pfa.regions == 0) die();
-}
-
-
-
-
-_PREINIT_
-static void alloc_init(struct multiboot_tag_mmap *map)
-{
-        alloc_preinit(map);
-
-        if (unlikely(preinit_pfa.regions == 0)) die();
-
-        physmem_region_t *r = &preinit_pfa.region[preinit_pfa.regions - 1];
-
-        page_frame_t *last_frame = (page_frame_t *)r->base + (r->max_frames - 1);
-        page_frame_t *first_frame = (page_frame_t *)r->base;
         
-        first_frame->next = KPAGE_ERR;
-
-        for (page_frame_t *pf = first_frame + 1; pf <= last_frame; pf++) {
-                pf->next = pf - 1;
-        }
-
-        r->top = last_frame;
+        sort_region_addrs(preinit_pfa.region, preinit_pfa.region_count);
 }
 
 
 
 
-_PREINIT_ __attribute__((malloc))
+_preinit_ _malloc_ _assume_aligned_(4096)
 static void *alloc_frame(void)
 {
-        physmem_region_t *r = &preinit_pfa.region[0];
-        page_frame_t *p = r->top;
+        physmem_pregion_t **pr = &preinit_pfa.region[0];
+        page_frame_t *frame = KPAGE_ERR;
 
-        /* 0x0 is a valid phys addr we can usually use, so we need to use something else. */
-        if (unlikely(p == KPAGE_ERR)) {
-                return KPAGE_ERR;
+
+        for (; *pr != KPAGE_ERR; pr++) {
+                physmem_pregion_t *r = *pr;
+
+                if (r->cur_frames == 0) continue;
+                
+                frame = r->top;
+
+                r->top--;
+                r->cur_frames--;
+
+                pi_memset(frame, 0, PAGESIZE);
+                return frame;
         }
 
-        r->top = p->next;
-        pi_memset(p, 0, PAGESIZE);
-        r->cur_frames--;
-
-        return p;
+        return KPAGE_ERR;
 }
 
 
 
 
 /* If a pagetable is present in the PTE, returns it, if not, allocates, sets and returns it. */
-_PREINIT_ __attribute__((nonnull(1)))
+_preinit_ _nonnull_(1)
 static PageTable *chk_pt_alloc(u64 *const restrict pte)
 {
         PageTable *pagetbl;
@@ -267,17 +267,12 @@ static constexpr usize PT_L4_ENTRYMASK = PT_L4_ENTRYSZ - 1;
 
 
 
-_PREINIT_ __attribute__((nonnull(1)))
+_preinit_ _nonnull_(1)
 static void pt_init_dram_map(PageTable *const pt_l4)
 {
-        extern char KERNEL_PHMMAP[];
+        void *dram_map_offs = (void *)get_kernel_phmap();
 
-        void *dram_map_offs;
-
-        /* fuck you, compiler. */
-        __asm__ volatile ("movabsq $KERNEL_PHMMAP, %0" : "=r"((u64)dram_map_offs) : : "memory");
-
-	const usize ramsz = preinit_pfa.usable_ram;
+	const usize ramsz = preinit_pfa.total_ram;
 
 	const usize pt_l3_count = ((ramsz + PT_L4_ENTRYMASK) & ~PT_L4_ENTRYMASK) / PT_L4_ENTRYSZ;
         const usize pt_l2_count = ((ramsz + PT_L3_ENTRYMASK) & ~PT_L3_ENTRYMASK) / PT_L3_ENTRYSZ;
@@ -294,7 +289,7 @@ static void pt_init_dram_map(PageTable *const pt_l4)
 
                 for (usize j = 0; j < cnt; j++) {
                         const u16 l3_entry_idx = page_l3_idx(dram_map_offs);
-                        pt_l3->entries[l3_entry_idx] = ((u64)physbase | PT_FLAGS_RW);
+                        pt_l3->entries[l3_entry_idx] = ((u64)physbase | PT_FLAGS_RW | PT_FLAG_PAGESIZE);
 
                         physbase += PT_L3_ENTRYSZ;
                         dram_map_offs = (u8 *)dram_map_offs + PT_L3_ENTRYSZ;
@@ -303,7 +298,7 @@ static void pt_init_dram_map(PageTable *const pt_l4)
                 rem -= cnt;
         }
 }
-_PREINIT_ __attribute__((nonnull(1)))
+_preinit_ _nonnull_(1)
 static void pt_init_kcode(PageTable *const pt_l4)
 {
         /* this is basically just another direct map, --------- *
@@ -311,7 +306,7 @@ static void pt_init_kcode(PageTable *const pt_l4)
         const u64 kernsz = (u64)_kphys_end - (u64)_kphys_start;
         const u64 pt_l1_count = ((kernsz + PT_L2_ENTRYMASK) & ~PT_L2_ENTRYMASK) / PT_L2_ENTRYSZ;
 
-        if (pt_l1_count > PT_ENTRIES) die();
+        if (pt_l1_count > PT_ENTRIES) die("pt_init_kcode() needs some refactoring!");
 
         void *koffs = (void *)KERNEL_OFFSET;
 
@@ -339,13 +334,13 @@ static void pt_init_kcode(PageTable *const pt_l4)
 
         
 }
-_PREINIT_ __attribute__((nonnull(1)))
+_preinit_ _nonnull_(1)
 static void pt_init_temp_identmap(PageTable *pt_l4)
 {
         u64 *const restrict pt_l4_entry = &pt_l4->entries[0];
         PageTable *pt_l3 = chk_pt_alloc(pt_l4_entry);
 
-        if (unlikely(pt_l3 == KPAGE_ERR)) die();
+        if (unlikely(pt_l3 == KPAGE_ERR)) die("pt_l3 is an invalid ptr!!");
 
         pt_l4->entries[0] = (u64)pt_l3 | PT_FLAGS_RW;
         pt_l3->entries[0] = (0x0ULL | PT_FLAGS_RW | PT_FLAG_PAGESIZE);
@@ -355,7 +350,7 @@ static void pt_init_temp_identmap(PageTable *pt_l4)
 
 
 /* pagetables are the bane of my existence. */
-_PREINIT_
+_preinit_
 static void init_higher_half(void)
 {
         PageTable *pt_l4 = alloc_frame();
@@ -369,14 +364,35 @@ static void init_higher_half(void)
 
 
 
-_PREINIT_
-_Noreturn void start_kernel(void *mbh)
+_preinit_
+static void buddy_handoff(void)
+{
+        MASTER_BUDDY.total_ram = preinit_pfa.total_ram;
+        MASTER_BUDDY.usable_ram = preinit_pfa.usable_ram;
+
+        for (usize i = 0; i < preinit_pfa.region_count; i++) {
+                physmem_pregion_t *r = preinit_pfa.region[i];
+
+                void *newbase = phys_to_virt_pm(r->base);
+                usize len = r->cur_frames * 4096;
+
+                pi_memset(newbase, 0, sizeof(slave_buddy_t));
+                kbud_free_range((phys_addr_t)newbase, len);
+        }
+}
+
+
+
+
+_preinit_ _noreturn_
+void start_kernel(void *mbh)
 {
         struct multiboot_tag_mmap *map = (void *)find_mbt(mbh, MULTIBOOT_TAG_TYPE_MMAP);
         multiboot2_tmmap = map;
 
         alloc_init(map);
         init_higher_half();
+        buddy_handoff();
         trampoline();
 }
 
--- src/kernel/boot.h
+++ src/kernel/boot.h
@@ -3,11 +3,33 @@
 
 #pragma once
 
+#include "macros.h"
 #include "types.h"
 #include "pframe.h"
 #include "multiboot2.h"
 
+typedef struct preinit_region {
+        page_frame_t *top;
 
-extern struct kpage_core preinit_pfa;
+        // usually points to the physmem_pregion_t.
+        void *base;
+
+        usize cur_frames;
+        usize max_frames;
+} physmem_pregion_t;
+
+struct preinit_allocator {
+        // KPAGE_ERR/(void *)-1 is the terminator.
+        physmem_pregion_t *region[PFRAME_MAX_REGIONS];
+
+        usize region_count;
+
+        // includes unusable ram.
+        usize total_ram;
+        usize usable_ram;
+
+};
+
+extern struct preinit_allocator preinit_pfa;
 extern struct multiboot_tag_mmap *multiboot2_tmmap;
 
